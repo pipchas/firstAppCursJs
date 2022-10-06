657441c87eec072a1a9c0420c4f6a0aa4e779714
pipoc <seva.bulava@mail.ru>
2022-10-06 13:54:27 +0300

    1st task done

--- scripts/1scripts.js
+++ scripts/1scripts.js
@@ -1,4 +1,6 @@
-let counstFilms = prompt('How many films do you watch?', '');
+"use strict";
+
+const counstFilms = +prompt('How many films do you watch?', '');
 
 const personalMovieDB = {
     count: counstFilms,
@@ -8,14 +10,28 @@ const personalMovieDB = {
     privat: false,
 }
 
-const a = prompt('How last films do you watch?', '');
-        b = prompt('How do you rate this film?', ''),
-        c = prompt('How last films do you watch?', ''),
-        d = prompt('How do you rate this film?', '');
+for(let i = 0; i < 2; i++) {
+    const a = prompt('How last films do you watch?', '');
+    const b = prompt('How do you rate this film?', '');
 
-personalMovieDB[a] = b;
-personalMovieDB[c] = d;
-
+    if(a != null && a != '' && a.length < 50 && b != null && b != ''){
+        personalMovieDB.movies[a] = b;
+        console.log('Data confirm');
+    } else {
+        console.log('Error');
+        i--;
+    }
+}
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+    alert('Просмотрено слишком мало фильмов!');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30 ) {
+    alert('Вы киноман');
+} else {
+    alert('Произошла ошибка!');
+}
+
+
 
